560072b96ef7c1803f15b28e2eabc103ca873749
YOU PAN <sunnypanyou@gmail.com>
2023-02-23 20:50:36 -0500

    finish the improve

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,17 +1,15 @@
 class ReviewsController < ApplicationController
   before_action :set_restaurant, only: [ :new, :create ]
 
-  def new
-    @review = Review.new
-  end
-
   def create
     @review = Review.new(review_params)
     @review.restaurant = @restaurant
     if @review.save
+      # another way
+      # redirect_to '/restaurants/restaurant/@restaurant'
       redirect_to restaurant_path(@restaurant)
     else
-      render :new, status: :unprocessable_entity
+      render 'restaurants/show', status: 422
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   resources :restaurants, only: [:index, :show, :new, :create] do
-    resources :reviews, only: [:new, :create]
+    resources :reviews, only: [:create]
   end
   # resources :reviews, only: [:index]
 end
